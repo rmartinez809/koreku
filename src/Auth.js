3f66b712aed224a2d52c5bf85ebd4d63a8cb468b
RicardoMartinez.Denver@gmail.com <RicardoMartinez.Denver@gmail.com>
2022-11-19 16:17:39 -0700

    improve styling on login page

--- src/Auth.js
+++ src/Auth.js
@@ -2,6 +2,7 @@ import { Fragment, useState } from "react";
 import { supabase } from "./supabaseClient";
 import React from "react";
 import { useLocation, Link } from "react-router-dom";
+import googleIcon from './icons/google.png';
 
 export default function Auth() {
   const [email, setEmail] = useState('');
@@ -71,16 +72,27 @@ export default function Auth() {
         <h1 className="">Koreku</h1>
         <p>A new way to organize your collection</p>
       </div>
-      <div className="card login">
-        <div className="card-body d-grid gap-2">
-          <form id="login" className="d-grid gap-3"
+      <div className="card login-card card-bg-color">
+        <div className="card-body d-grid gap-2 center-text">
+          <form
+            id="login"
+            className="d-grid gap-3"
             onSubmit={(location.pathname === '/register') ? handleSignUp : handleLogin}>
-            <input type="email" className="form-control" id="email" placeholder="Email" value={email}
+            <input type="email"
+            className="form-control input-bg"
+            id="email"
+            placeholder="Email"
+            value={email} required
               onChange={(event) => {
                 setEmail(event.target.value);
               }}
             ></input>
-            <input type="password" className="form-control" id="password" placeholder="Password" minLength={minPassLen} value={password}
+            <input type="password"
+            className="form-control input-bg"
+            id="password"
+            placeholder="Password"
+            minLength={minPassLen}
+            value={password} required
               onChange={(event) => {
                 setPassword(event.target.value);
               }}
@@ -90,23 +102,32 @@ export default function Auth() {
             */}
             {location.pathname === "/register" ?
               <Fragment>
-                <input type="password" className="form-control" id="confirmPass" placeholder="Confirm Password" value={confirmPass}
+                <input type="password"
+                className="form-control input-bg"
+                id="confirmPass"
+                placeholder="Confirm Password"
+                value={confirmPass}
+                required
                 onChange={(event) => {
                   setConfirmPass(event.target.value);
                 }}></input>
-                <button type="submit" className="btn btn-primary" id="register-btn">Sign Up</button>
+                <button type="submit"
+                className="btn btn-primary"
+                id="register-btn">Sign Up</button>
               </Fragment>
-              : <button type="submit" className="btn btn-primary" id="login-btn">Log In</button>}
+              : <button type="submit"
+                className="btn btn-primary"
+                id="login-btn">Log In</button>}
           </form>
           <hr></hr>
-          <p>Or login with</p>
+          <p className="secondary">Or login with</p>
           <div className="d-grid gap-2">
-            <button className="btn btn-secondary" id="btn-Google"
-            onClick={signInWithGoogle}
-              >Google</button>
+            <button className="btn btn-secondary"
+            id="btn-Google"
+            onClick={signInWithGoogle}><img src={googleIcon} id="google-icon"></img>Google</button>
           </div>
             {location.pathname === "/" ?
-            <p>Not a member? <Link to="/register">Sign up</Link></p> : <p>Already have an account? <Link to="/">Log In</Link></p>}
+            <p className="secondary">Not a member? <Link to="/register">Sign up</Link></p> : <p className="secondary">Already have an account? <Link to="/">Log In</Link></p>}
         </div>
       </div>
       </main>
